e15589895f13bbf6a17051697e26f71cd32b39c0
Sebin Suresh <sebinsuresh@gmail.com>
2023-05-20 10:34:58 -0500

    Skeleton for adding TF to codebase

---
+++ README.md
@@ -0,0 +1,2 @@
+## About
+Experimenting with Serverless on AWS, Terraform, and Github Actions to create a simple web application.
---
+++ infra/main.tf
@@ -0,0 +1,9 @@
+# Need:
+# - DynamoDB
+# - Lambda functions
+#   - Read and zip from ../backend/lambdas ?
+#   - Zip module
+#   - S3 for backend
+#   - IAM roles for lambda to CRUD DynamoDB
+# - API Gateway
+# - Cloudwatch maybe for alerts and shutdown
