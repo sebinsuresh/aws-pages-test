0a70aea5a1fe461223680779aef76441cc024fd0
Sebin Suresh <sebinsuresh@gmail.com>
2023-05-20 14:37:58 -0500

    WIP: Additional changes for backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,15 @@
 provider "aws" {
-  region = "us-east-2"
+  region = var.region
+  # access_key                  = "fake"
+  # secret_key                  = "fake"
+  # skip_credentials_validation = true
+  # skip_metadata_api_check     = true
+  # skip_requesting_account_id  = true
+  # s3_force_path_style         = true
+  # endpoints {
+  #   dynamodb = "http://localhost:4566"
+  #   kinesis  = "http://localhost:4566"
+  #   lambda   = "http://localhost:4566"
+  #   s3       = "http://localhost:4566"
+  # }
 }
