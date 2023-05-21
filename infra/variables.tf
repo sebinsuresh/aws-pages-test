7149eef1c5798ff09be7c7225cd94323eca0ed97
Sebin Suresh <sebinsuresh@gmail.com>
2023-05-20 23:27:46 -0500

    Use local specific variables to set values in provider

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,15 @@ variable "aws_region" {
   type        = string
   description = "region to deploy the infrastructure"
 }
+
+variable "is_local" {
+  type        = bool
+  description = "is the infrastructure being deployed locally"
+  default     = false
+}
+
+variable "localstack_endpoint" {
+  type        = string
+  description = "localstack endpoint to be used when deploying locally"
+  default     = "http://localhost:4566"
+}
